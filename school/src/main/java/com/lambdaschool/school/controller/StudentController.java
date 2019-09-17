8ec8b8cced7efd78cfbecd128ae19c48aa5cc349
Jonathan Scott <jonathan.scott@gmail.com>
2019-09-17 14:12:37 -0700

    mvp finished

--- school/src/main/java/com/lambdaschool/school/controller/StudentController.java
+++ school/src/main/java/com/lambdaschool/school/controller/StudentController.java
@@ -1,14 +1,21 @@
 package com.lambdaschool.school.controller;
 
+import com.lambdaschool.school.model.ErrorDetail;
 import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.service.StudentService;
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -18,46 +25,78 @@ import java.util.List;
 @RequestMapping("/students")
 public class StudentController
 {
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @Autowired
     private StudentService studentService;
 
-    // Please note there is no way to add students to course yet!
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integr", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")})
 
+    // Please note there is no way to add students to course yet!
+    // GET localhost:2019/students/student/1
+    @ApiOperation(value = "Get All Students", response = Student.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Students Found", response = Student.class),
+            @ApiResponse(code = 404, message = "Students Not Found", response = ErrorDetail.class)})
     @GetMapping(value = "/students", produces = {"application/json"})
-    public ResponseEntity<?> listAllStudents()
+    public ResponseEntity<?> listAllStudents(HttpServletRequest request, @PageableDefault(page = 0, size= 3, sort = "studname")
+            Pageable pageable)
     {
-        List<Student> myStudents = studentService.findAll();
+        logger.warn(request.getMethod().toUpperCase() + " " + request.getRequestURI() + " accessed");
+
+        List<Student> myStudents = studentService.findAll(pageable);
         return new ResponseEntity<>(myStudents, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get a Student by ID", response = Student.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Student Found", response = Student.class),
+            @ApiResponse(code = 404, message = "Student Not Found", response = ErrorDetail.class)})
     @GetMapping(value = "/Student/{StudentId}",
                 produces = {"application/json"})
-    public ResponseEntity<?> getStudentById(
+    public ResponseEntity<?> getStudentById(HttpServletRequest request,
             @PathVariable
                     Long StudentId)
     {
+        logger.warn(request.getMethod().toUpperCase() + " " + request.getRequestURI() + " accessed");
+
         Student r = studentService.findStudentById(StudentId);
         return new ResponseEntity<>(r, HttpStatus.OK);
     }
 
 
+    @ApiOperation(value = "Get a Student by Name", response = Student.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Student Found", response = Student.class),
+            @ApiResponse(code = 404, message = "Student Not Found", response = ErrorDetail.class)})
     @GetMapping(value = "/student/namelike/{name}",
                 produces = {"application/json"})
-    public ResponseEntity<?> getStudentByNameContaining(
+    public ResponseEntity<?> getStudentByNameContaining(HttpServletRequest request,
             @PathVariable String name)
     {
+        logger.warn(request.getMethod().toUpperCase() + " " + request.getRequestURI() + " accessed");
+
         List<Student> myStudents = studentService.findStudentByNameLike(name);
         return new ResponseEntity<>(myStudents, HttpStatus.OK);
     }
 
 
+    @ApiOperation(value = "Create a new Student", notes = "The newly created Student id will be sent in the location header", response = void.class)
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Student Created", response = void.class),
+            @ApiResponse(code = 500, message = "Student Creation Failed", response = ErrorDetail.class)})
     @PostMapping(value = "/Student",
                  consumes = {"application/json"},
                  produces = {"application/json"})
-    public ResponseEntity<?> addNewStudent(@Valid
+    public ResponseEntity<?> addNewStudent(HttpServletRequest request, @Valid
                                            @RequestBody
                                                    Student newStudent) throws URISyntaxException
     {
+        logger.warn(request.getMethod().toUpperCase() + " " + request.getRequestURI() + " accessed. " + newStudent + " sent");
         newStudent = studentService.save(newStudent);
 
         // set the location header for the newly created resource
@@ -69,23 +108,32 @@ public class StudentController
     }
 
 
+    @ApiOperation(value = "Edit a Student based on Id", response = void.class)
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Student Edited", response = void.class),
+            @ApiResponse(code = 500, message = "Student Editing Failed", response = ErrorDetail.class)})
     @PutMapping(value = "/Student/{Studentid}")
-    public ResponseEntity<?> updateStudent(
+    public ResponseEntity<?> updateStudent(HttpServletRequest request,
             @RequestBody
                     Student updateStudent,
             @PathVariable
                     long Studentid)
     {
+        logger.warn(request.getMethod().toUpperCase() + " " + request.getRequestURI() + " accessed. " + updateStudent + " and " + Studentid + " sent");
         studentService.update(updateStudent, Studentid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
+    @ApiOperation(value = "Delete a Student based on Id", response = void.class)
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Student Edited", response = void.class),
+            @ApiResponse(code = 500, message = "Student Delete Failed", response = ErrorDetail.class)})
     @DeleteMapping("/Student/{Studentid}")
-    public ResponseEntity<?> deleteStudentById(
+    public ResponseEntity<?> deleteStudentById(HttpServletRequest request,
             @PathVariable
                     long Studentid)
     {
+        logger.warn(request.getMethod().toUpperCase() + " " + request.getRequestURI() + " accessed. " + " and " + Studentid + " sent");
+
         studentService.delete(Studentid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
